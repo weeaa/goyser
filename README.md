c4f0f1e78802d5e2fd1e7d459627be097ec7c7f4
weeaa <github@ryanriviere.com>
2024-06-21 01:54:23 +0200

    🦺 Added example & lot of methods

--- README.md
+++ README.md
@@ -1,8 +1,117 @@
 # Geyser Yellowstone SDK
 [![GoDoc](https://pkg.go.dev/badge/github.com/weeaa/goyser?status.svg)](https://pkg.go.dev/github.com/weeaa/goyser?tab=doc)
-[![Go Report Card](https://goreportcard.com/badge/github.com/weeaa/goyser)](https://goreportcard.com/report/github.com/weeaa/jito-go)
+[![Go Report Card](https://goreportcard.com/badge/github.com/weeaa/goyser)](https://goreportcard.com/report/github.com/weeaa/goyser)
 [![License](https://img.shields.io/badge/license-Apache_2.0-crimson)](https://opensource.org/license/apache-2-0)
 
-This library contains tooling to interact with **[Yellowstone Geyser Plugin](https://docs.solanalabs.com/validator/geyser)**.
+This library contains tooling to interact with **[Yellowstone Geyser Plugin](https://docs.solanalabs.com/validator/geyser)**. Work in progress. 👷
 
 ![yellowstone](https://files.oaiusercontent.com/file-cg9YkbHcSC5kXatlKK6WLG7i?se=2024-06-20T11%3A48%3A12Z&sp=r&sv=2023-11-03&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3D198eb09f-2fb3-4e19-9ff2-f9c813cddeb9.webp&sig=19er0erTrnglRhYISe/nJLOoubqswjgo71so/JVadyI%3D)
+
+## ❇️ Contents
+- [Methods](#-methods)
+- [Installing](#-installing)
+- [Examples](#-examples)
+  - [Subscribe to Slots & Account](#subscribe-to-slots-and-account)
+- [Disclaimer](#-disclaimer)
+- [Support](#-support)
+- [License](#-license)
+
+## 📡 Methods
+- `SubscribeAccounts`
+  - `AppendAccounts`
+  - `UnsubscribeAccounts`
+  - `UnsubscribeAccountsByID`
+- `SubscribeSlots`
+  - `UnsubscribeSlots`
+- `SubscribeTransaction`
+  - `UnsubscribeTransaction`
+- `SubscribeTransactionStatus`
+- `SubscribeBlocks`
+- `SubscribeBlocksMeta`
+- `SubscribeEntry`
+- `SubscribeAccountDataSlice`
+
+## 💾 Installing
+
+Go 1.22.0 or higher.
+```shell
+go get github.com/weeaa/goyser@latest
+```
+
+## 💻 Examples
+
+### `Subscribe to Slots and Account`
+```go
+package main
+
+import (
+  "context"
+  "github.com/joho/godotenv"
+  "github.com/weeaa/goyser"
+  "github.com/weeaa/goyser/pb"
+  "log"
+  "os"
+)
+
+func main() {
+  ctx := context.Background()
+
+  if err := godotenv.Load(); err != nil {
+    log.Fatal(err)
+  }
+
+  rpcAddr, ok := os.LookupEnv("GEYSER_RPC")
+  if !ok {
+    log.Fatal("could not inf GEYSER_RPC in .env")
+  }
+
+  client, err := goyser.New(
+    ctx,
+    rpcAddr,
+  )
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer client.GrpcConn.Close()
+
+  if err = client.NewSubscribeClient("main", ctx); err != nil {
+    log.Fatal(err)
+  }
+  defer client.DefaultStreamClient.Geyser.CloseSend()
+
+  stream := client.Streams["main"]
+  defer client.DefaultStreamClient.Geyser.CloseSend()
+
+  if err = stream.SubscribeSlots("slots", &pb.SubscribeRequestFilterSlots{}); err != nil {
+    log.Fatal(err)
+  }
+  if err = stream.SubscribeAccounts("accounts", &pb.SubscribeRequestFilterAccounts{
+    Account: []string{"EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"},
+  }); err != nil {
+    log.Fatal(err)
+  }
+
+  for out := range stream.Ch {
+    switch {
+    case out.GetSlot() != nil:
+      log.Printf("slot updata: %+v", out.GetSlot())
+    case out.GetTransaction() != nil:
+      log.Printf("tx updata: %+v", out.GetTransaction())
+    default:
+      log.Printf("other updqte: %+v", out)
+    }
+  }
+}
+```
+
+## 🚨 Disclaimer
+
+**This library is not affiliated with Jito Labs**. It is a community project and is not officially supported by Jito Labs. Use at your own risk.
+
+## 🛟 Support
+If my work has been useful in building your for-profit services/infra/bots/etc, consider donating at
+`EcrHvqa5Vh4NhR3bitRZVrdcUGr1Z3o6bXHz7xgBU2FB` (SOL).
+
+## 📃 License
+
+[Apache-2.0 License](https://github.com/weeaa/jito-go/blob/main/LICENSE).
--- geyser.go
+++ geyser.go
@@ -5,29 +5,26 @@ import (
 	"fmt"
 	"github.com/weeaa/goyser/pb"
 	"google.golang.org/grpc"
+	"slices"
 	"sync"
 )
 
 type Client struct {
-	GrpcConn *grpc.ClientConn
-	Ctx      context.Context
-
-	Geyser               pb.GeyserClient
-	Streams              map[string]*StreamClient
-	DefaultStreamClient  *StreamClient
-	DefaultAccountFilter *pb.SubscribeRequestFilterAccounts
-	mu                   sync.Mutex
-
-	ErrCh chan error
+	GrpcConn            *grpc.ClientConn         // gRPC connection
+	Ctx                 context.Context          // Context for cancellation and deadlines
+	Geyser              pb.GeyserClient          // Geyser client
+	Streams             map[string]*StreamClient // Active stream clients
+	DefaultStreamClient *StreamClient            // Default stream client
+	mu                  sync.Mutex               // Mutex for thread safety
+	ErrCh               chan error               // Channel for errors
 }
 
 type StreamClient struct {
-	Ctx     context.Context
-	Geyser  pb.Geyser_SubscribeClient
-	Request *pb.SubscribeRequest
-
-	Ch    <-chan *pb.SubscribeUpdate
-	ErrCh <-chan error
+	Ctx     context.Context            // Context for cancellation and deadlines
+	Geyser  pb.Geyser_SubscribeClient  // Geyser subscribe client
+	Request *pb.SubscribeRequest       // Subscribe request
+	Ch      <-chan *pb.SubscribeUpdate // Channel for updates
+	ErrCh   <-chan error               // Channel for errors
 }
 
 func New(ctx context.Context, grpcDialURL string) (*Client, error) {
@@ -92,6 +89,7 @@ func (c *Client) NewSubscribeClient(name string, ctx context.Context) error {
 	return nil
 }
 
+// SetDefaultSubscribeClient sets the default subscribe client.
 func (c *Client) SetDefaultSubscribeClient(client pb.Geyser_SubscribeClient) *Client {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -99,46 +97,87 @@ func (c *Client) SetDefaultSubscribeClient(client pb.Geyser_SubscribeClient) *Cl
 	return c
 }
 
+// SubscribeAccounts subscribes to account updates.
+// Note: This will overwrite existing subscriptions for the given ID.
+// To add new accounts without overwriting, use AppendAccounts.
 func (s *StreamClient) SubscribeAccounts(id string, req *pb.SubscribeRequestFilterAccounts) error {
 	s.Request.Accounts[id] = req
 	return s.Geyser.Send(s.Request)
 }
 
+// AppendAccounts appends accounts to an existing subscription and sends the request.
+func (s *StreamClient) AppendAccounts(id string, accounts ...string) error {
+	s.Request.Accounts[id].Account = append(s.Request.Accounts[id].Account, accounts...)
+	return s.Geyser.Send(s.Request)
+}
+
+// UnsubscribeAccountsByID unsubscribes from account updates by ID.
+func (s *StreamClient) UnsubscribeAccountsByID(id string) {
+	delete(s.Request.Accounts, id)
+}
+
+// UnsubscribeAccounts unsubscribes specific accounts.
+func (s *StreamClient) UnsubscribeAccounts(id string, accounts ...string) {
+	for _, account := range accounts {
+		s.Request.Accounts[id].Account = slices.DeleteFunc(s.Request.Accounts[id].Account, func(a string) bool {
+			return a == account
+		})
+	}
+}
+
+// SubscribeSlots subscribes to slot updates.
 func (s *StreamClient) SubscribeSlots(id string, req *pb.SubscribeRequestFilterSlots) error {
 	s.Request.Slots[id] = req
 	return s.Geyser.Send(s.Request)
 }
 
+// UnsubscribeSlots unsubscribes from slot updates.
+func (s *StreamClient) UnsubscribeSlots(id string) {
+	delete(s.Request.Slots, id)
+}
+
+// SubscribeTransaction subscribes to transaction updates.
 func (s *StreamClient) SubscribeTransaction(id string, req *pb.SubscribeRequestFilterTransactions) error {
 	s.Request.Transactions[id] = req
 	return s.Geyser.Send(s.Request)
 }
 
+// UnsubscribeTransaction unsubscribes from transaction updates.
+func (s *StreamClient) UnsubscribeTransaction(id string) {
+	delete(s.Request.Transactions, id)
+}
+
+// SubscribeTransactionStatus subscribes to transaction status updates.
 func (s *StreamClient) SubscribeTransactionStatus(id string, req *pb.SubscribeRequestFilterTransactions) error {
 	s.Request.TransactionsStatus[id] = req
 	return s.Geyser.Send(s.Request)
 }
 
+// SubscribeBlocks subscribes to block updates.
 func (s *StreamClient) SubscribeBlocks(id string, req *pb.SubscribeRequestFilterBlocks) error {
 	s.Request.Blocks[id] = req
 	return s.Geyser.Send(s.Request)
 }
 
+// SubscribeBlocksMeta subscribes to block metadata updates.
 func (s *StreamClient) SubscribeBlocksMeta(id string, req *pb.SubscribeRequestFilterBlocksMeta) error {
 	s.Request.BlocksMeta[id] = req
 	return s.Geyser.Send(s.Request)
 }
 
+// SubscribeEntry subscribes to entry updates.
 func (s *StreamClient) SubscribeEntry(id string, req *pb.SubscribeRequestFilterEntry) error {
 	s.Request.Entry[id] = req
 	return s.Geyser.Send(s.Request)
 }
 
+// SubscribeAccountDataSlice subscribes to account data slice updates.
 func (s *StreamClient) SubscribeAccountDataSlice(req []*pb.SubscribeRequestAccountsDataSlice) error {
 	s.Request.AccountsDataSlice = req
 	return s.Geyser.Send(s.Request)
 }
 
+// listen starts listening for responses and errors.
 func (s *StreamClient) listen() {
 	s.Ch, s.ErrCh = s.Geyser.Response()
 }
