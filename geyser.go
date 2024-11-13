8ae6bc1a801492afb38d8fb9b3fb7dcc1e23725b
weeaa <github@ryanriviere.com>
2024-11-13 17:55:08 +0100

    update ConvertTransaction to include extended metadata and account details

--- geyser.go
+++ geyser.go
@@ -2,14 +2,15 @@ package goyser
 
 import (
 	"context"
-	"google.golang.org/grpc/metadata"
-	"io"
-
+	"encoding/binary"
+	"errors"
 	"fmt"
 	"github.com/gagliardetto/solana-go"
 	"github.com/gagliardetto/solana-go/rpc"
 	"github.com/weeaa/goyser/pb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+	"io"
 	"slices"
 	"strconv"
 	"sync"
@@ -37,6 +38,7 @@ type StreamClient struct {
 	mu      sync.RWMutex
 }
 
+// New creates a new Client instance.
 func New(ctx context.Context, grpcDialURL string, md metadata.MD) (*Client, error) {
 	ch := make(chan error)
 	conn, err := createAndObserveGRPCConn(ctx, ch, grpcDialURL, md)
@@ -62,7 +64,11 @@ func New(ctx context.Context, grpcDialURL string, md metadata.MD) (*Client, erro
 	}, nil
 }
 
+// Close closes the client and all the streams.
 func (c *Client) Close() error {
+	for _, sc := range c.s.clients {
+		sc.Stop()
+	}
 	return c.grpcConn.Close()
 }
 
@@ -70,7 +76,7 @@ func (c *Client) Ping(count int32) (*geyser_pb.PongResponse, error) {
 	return c.Geyser.Ping(c.Ctx, &geyser_pb.PingRequest{Count: count})
 }
 
-// AddStreamClient creates a new Geyser subscribe stream client.
+// AddStreamClient creates a new Geyser subscribe stream client. You can then retrieve it with GetStreamClient
 func (c *Client) AddStreamClient(ctx context.Context, streamName string, opts ...grpc.CallOption) error {
 	c.s.mu.Lock()
 	defer c.s.mu.Unlock()
@@ -114,7 +120,7 @@ func (s *StreamClient) Stop() {
 	close(s.ErrCh)
 }
 
-// GetStreamClient Returns a StreamClient for the given streamName from the client's map.
+// GetStreamClient returns a StreamClient for the given streamName from the client's map.
 func (c *Client) GetStreamClient(streamName string) *StreamClient {
 	defer c.s.mu.RUnlock()
 	c.s.mu.RLock()
@@ -161,6 +167,7 @@ func (s *StreamClient) SubscribeAccounts(filterName string, req *geyser_pb.Subsc
 	return s.geyser.Send(s.request)
 }
 
+// GetAccounts returns all account addresses for the given filter name.
 func (s *StreamClient) GetAccounts(filterName string) []string {
 	defer s.mu.RUnlock()
 	s.mu.RLock()
@@ -173,8 +180,8 @@ func (s *StreamClient) AppendAccounts(filterName string, accounts ...string) err
 	return s.geyser.Send(s.request)
 }
 
-// UnsubscribeAccountsByID unsubscribes from account updates by ID.
-func (s *StreamClient) UnsubscribeAccountsByID(filterName string) error {
+// UnsubscribeAccountsByFilterName unsubscribes from account updates by filter name.
+func (s *StreamClient) UnsubscribeAccountsByFilterName(filterName string) error {
 	delete(s.request.Accounts, filterName)
 	return s.geyser.Send(s.request)
 }
@@ -291,11 +298,15 @@ func (s *StreamClient) listen() {
 			recv, err := s.geyser.Recv()
 			if err != nil {
 				if err == io.EOF {
+					s.ErrCh <- errors.New("stream cancelled: EOF")
 					return
 				}
 				select {
 				case s.ErrCh <- fmt.Errorf("error receiving from stream: %w", err):
 				case <-s.Ctx.Done():
+					if err = s.Ctx.Err(); err != nil {
+						s.ErrCh <- fmt.Errorf("stream context cancelled: %w", err)
+					}
 					return
 				}
 				return
@@ -309,6 +320,128 @@ func (s *StreamClient) listen() {
 	}
 }
 
+// ConvertTransaction converts a Geyser parsed transaction into an rpc.GetTransactionResult format.
+func ConvertTransaction(geyserTx *geyser_pb.SubscribeUpdateTransaction) (*rpc.GetTransactionResult, error) {
+	var tx rpc.GetTransactionResult
+
+	meta := geyserTx.Transaction.Meta
+	transaction := geyserTx.Transaction.Transaction
+
+	tx.Meta.PreBalances = meta.PreBalances
+	tx.Meta.PostBalances = meta.PostBalances
+	tx.Meta.Err = meta.Err
+	tx.Meta.Fee = meta.Fee
+	tx.Meta.ComputeUnitsConsumed = meta.ComputeUnitsConsumed
+	tx.Meta.LogMessages = meta.LogMessages
+
+	for _, preTokenBalance := range meta.PreTokenBalances {
+		owner := solana.MustPublicKeyFromBase58(preTokenBalance.Owner)
+		tx.Meta.PreTokenBalances = append(tx.Meta.PreTokenBalances, rpc.TokenBalance{
+			AccountIndex: uint16(preTokenBalance.AccountIndex),
+			Owner:        &owner,
+			Mint:         solana.MustPublicKeyFromBase58(preTokenBalance.Mint),
+			UiTokenAmount: &rpc.UiTokenAmount{
+				Amount:         preTokenBalance.UiTokenAmount.Amount,
+				Decimals:       uint8(preTokenBalance.UiTokenAmount.Decimals),
+				UiAmount:       &preTokenBalance.UiTokenAmount.UiAmount,
+				UiAmountString: preTokenBalance.UiTokenAmount.UiAmountString,
+			},
+		})
+	}
+
+	for _, postTokenBalance := range meta.PostTokenBalances {
+		owner := solana.MustPublicKeyFromBase58(postTokenBalance.Owner)
+		tx.Meta.PostTokenBalances = append(tx.Meta.PostTokenBalances, rpc.TokenBalance{
+			AccountIndex: uint16(postTokenBalance.AccountIndex),
+			Owner:        &owner,
+			Mint:         solana.MustPublicKeyFromBase58(postTokenBalance.Mint),
+			UiTokenAmount: &rpc.UiTokenAmount{
+				Amount:         postTokenBalance.UiTokenAmount.Amount,
+				Decimals:       uint8(postTokenBalance.UiTokenAmount.Decimals),
+				UiAmount:       &postTokenBalance.UiTokenAmount.UiAmount,
+				UiAmountString: postTokenBalance.UiTokenAmount.UiAmountString,
+			},
+		})
+	}
+
+	for i, innerInst := range meta.InnerInstructions {
+		tx.Meta.InnerInstructions[i].Index = uint16(innerInst.Index)
+		for x, inst := range innerInst.Instructions {
+			accounts, err := bytesToUint16Slice(inst.Accounts)
+			if err != nil {
+				return nil, err
+			}
+
+			tx.Meta.InnerInstructions[i].Instructions[x].Accounts = accounts
+			tx.Meta.InnerInstructions[i].Instructions[x].ProgramIDIndex = uint16(inst.ProgramIdIndex)
+			if err = tx.Meta.InnerInstructions[i].Instructions[x].Data.UnmarshalJSON(inst.Data); err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	for _, reward := range meta.Rewards {
+		comm, _ := strconv.ParseUint(reward.Commission, 10, 64)
+		commission := uint8(comm)
+		tx.Meta.Rewards = append(tx.Meta.Rewards, rpc.BlockReward{
+			Pubkey:      solana.MustPublicKeyFromBase58(reward.Pubkey),
+			Lamports:    reward.Lamports,
+			PostBalance: reward.PostBalance,
+			RewardType:  rpc.RewardType(reward.RewardType.String()),
+			Commission:  &commission,
+		})
+	}
+
+	for _, readOnlyAddress := range meta.LoadedReadonlyAddresses {
+		tx.Meta.LoadedAddresses.ReadOnly = append(tx.Meta.LoadedAddresses.ReadOnly, solana.PublicKeyFromBytes(readOnlyAddress))
+	}
+
+	for _, writableAddress := range meta.LoadedWritableAddresses {
+		tx.Meta.LoadedAddresses.ReadOnly = append(tx.Meta.LoadedAddresses.ReadOnly, solana.PublicKeyFromBytes(writableAddress))
+	}
+
+	solTx, err := tx.Transaction.GetTransaction()
+	if err != nil {
+		return nil, err
+	}
+
+	solTx.Message.RecentBlockhash = solana.HashFromBytes(transaction.Message.RecentBlockhash)
+	solTx.Message.Header = solana.MessageHeader{
+		NumRequiredSignatures:       uint8(transaction.Message.Header.NumRequiredSignatures),
+		NumReadonlySignedAccounts:   uint8(transaction.Message.Header.NumReadonlySignedAccounts),
+		NumReadonlyUnsignedAccounts: uint8(transaction.Message.Header.NumReadonlyUnsignedAccounts),
+	}
+
+	for _, sig := range transaction.Signatures {
+		solTx.Signatures = append(solTx.Signatures, solana.SignatureFromBytes(sig))
+	}
+
+	for _, table := range transaction.Message.AddressTableLookups {
+		solTx.Message.AddressTableLookups = append(solTx.Message.AddressTableLookups, solana.MessageAddressTableLookup{
+			AccountKey:      solana.PublicKeyFromBytes(table.AccountKey),
+			WritableIndexes: table.WritableIndexes,
+			ReadonlyIndexes: table.ReadonlyIndexes,
+		})
+	}
+
+	for _, inst := range transaction.Message.Instructions {
+		accounts, err := bytesToUint16Slice(inst.Accounts)
+		if err != nil {
+			return nil, err
+		}
+
+		solTx.Message.Instructions = append(solTx.Message.Instructions, solana.CompiledInstruction{
+			ProgramIDIndex: uint16(inst.ProgramIdIndex),
+			Accounts:       accounts,
+			Data:           inst.Data,
+		})
+	}
+
+	return &tx, nil
+}
+
+/*
+// Deprecated: Use ConvertParsedTransaction instead.
 func ConvertTransaction(geyserTx *geyser_pb.SubscribeUpdateTransaction) *solana.Transaction {
 	tx := new(solana.Transaction)
 
@@ -355,11 +488,16 @@ func ConvertTransaction(geyserTx *geyser_pb.SubscribeUpdateTransaction) *solana.
 
 	return tx
 }
+*/
 
-func BatchConvertTransaction(geyserTxns ...*geyser_pb.SubscribeUpdateTransaction) []*solana.Transaction {
-	txns := make([]*solana.Transaction, len(geyserTxns), 0)
+func BatchConvertTransaction(geyserTxns ...*geyser_pb.SubscribeUpdateTransaction) []*rpc.GetTransactionResult {
+	txns := make([]*rpc.GetTransactionResult, len(geyserTxns), 0)
 	for _, tx := range geyserTxns {
-		txns = append(txns, ConvertTransaction(tx))
+		txn, err := ConvertTransaction(tx)
+		if err != nil {
+			continue
+		}
+		txns = append(txns, txn)
 	}
 	return txns
 }
@@ -411,4 +549,18 @@ func BatchConvertBlockHash(geyserBlocks ...*geyser_pb.SubscribeUpdateBlock) []*r
 		blocks = append(blocks, ConvertBlockHash(block))
 	}
 	return blocks
+}
+
+func bytesToUint16Slice(data []byte) ([]uint16, error) {
+	if len(data)%2 != 0 {
+		return nil, fmt.Errorf("length of byte slice must be even to convert to uint16 slice")
+	}
+
+	uint16s := make([]uint16, len(data)/2)
+
+	for i := 0; i < len(data); i += 2 {
+		uint16s[i/2] = binary.LittleEndian.Uint16(data[i : i+2])
+	}
+
+	return uint16s, nil
 }
--- go.mod
+++ go.mod
@@ -1,12 +1,14 @@
 module github.com/weeaa/goyser
 
-go 1.22.1
+go 1.22.7
+
+toolchain go1.23.2
 
 require (
 	github.com/gagliardetto/solana-go v1.11.0
 	github.com/joho/godotenv v1.5.1
 	github.com/stretchr/testify v1.9.0
-	google.golang.org/grpc v1.67.1
+	google.golang.org/grpc v1.68.0
 	google.golang.org/protobuf v1.35.1
 )
 
@@ -16,7 +18,7 @@ require (
 	github.com/benbjohnson/clock v1.3.5 // indirect
 	github.com/blendle/zapdriver v1.3.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/fatih/color v1.17.0 // indirect
+	github.com/fatih/color v1.18.0 // indirect
 	github.com/gagliardetto/binary v0.8.0 // indirect
 	github.com/gagliardetto/treeout v0.1.4 // indirect
 	github.com/golang/mock v1.6.0 // indirect
@@ -33,17 +35,18 @@ require (
 	github.com/mr-tron/base58 v1.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/streamingfast/logging v0.0.0-20230608130331-f22c91403091 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	go.mongodb.org/mongo-driver v1.17.1 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/ratelimit v0.3.1 // indirect
 	go.uber.org/zap v1.27.0 // indirect
-	golang.org/x/crypto v0.28.0 // indirect
-	golang.org/x/net v0.30.0 // indirect
-	golang.org/x/sys v0.26.0 // indirect
-	golang.org/x/term v0.25.0 // indirect
-	golang.org/x/text v0.19.0 // indirect
-	golang.org/x/time v0.7.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20241007155032-5fefd90f89a9 // indirect
+	golang.org/x/crypto v0.29.0 // indirect
+	golang.org/x/net v0.31.0 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/term v0.26.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
+	golang.org/x/time v0.8.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241113154021-e0fbfb71d213 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
